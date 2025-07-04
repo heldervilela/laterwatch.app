287334f568b749322c7f9916f79868ba24843859
Helder Vilela <dev@heldervilela.com>
2025-07-04 14:19:03 +0000

    Refactor sidebar component by removing the NavSettings section and updating the sidebar width constants for improved layout. Replace the SidebarMenuButton with a BrandIconSmall for a cleaner design.

--- apps/app/src/routes/(app)/-ui/sidebar.tsx
+++ apps/app/src/routes/(app)/-ui/sidebar.tsx
@@ -1,18 +1,16 @@
-import { ArrowUpFromLine } from "lucide-react";
 import * as React from "react";
 
+import { BrandIconSmall } from "@/ui/assets/brand-icon-small";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
 } from "@/ui/base/sidebar";
 import { NavMain } from "./navigation/main";
 import { NavSecondary } from "./navigation/secondary";
-import { NavSettings } from "./navigation/settings";
 import { NavUser } from "./navigation/user";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -21,21 +19,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <a href="#">
-                <ArrowUpFromLine className="!size-5" />
-                <span className="text-base font-semibold">Vilela AI</span>
-              </a>
-            </SidebarMenuButton>
+            <BrandIconSmall className="size-7 ml-2" />
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
         <NavMain />
-        <NavSettings />
         <NavSecondary className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
